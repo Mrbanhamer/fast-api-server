334b6c06def1ad7420815fcc11f5292b19e800f5
SebRainDeer <sebastian@de-mare.se>
2026-03-09 10:22:42 +0100

    connected main.py with frontend

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fast-api-server
 tries putting all my coding skill to the test
+
+start the server with 'uvicron main:app'
+also has 'fastapi dev main.py'
--- main.py
+++ main.py
@@ -7,12 +7,17 @@ app = FastAPI()
 async def bye():
     return 'goodbye'
 
-@app.get('/index')
+@app.get('/index', response_class=HTMLResponse)
 async def hi():
-    return HTMLResponse('html-index/index.html')
+    try:
+        with open('front_end/index.h', 'r', encoding='utf-8') as f:
+            html_content = f.read()
+        return HTMLResponse(content=html_content, status_code=200)
+    except FileNotFoundError:
+        return HTMLResponse(content="<h1>File not found</h1>", status_code=404)
 
 
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000, log_level='info')
+    uvicorn.run(app, host='127.0.0.1', port=8000, log_level='info')
